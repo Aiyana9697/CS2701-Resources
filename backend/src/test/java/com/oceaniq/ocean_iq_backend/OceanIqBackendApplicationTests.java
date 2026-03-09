e0f549afb8b3db7f8aee0122a16560319bf27e6b
Aiyana Patel <aiyana.patel@outlook.com>
2026-03-09 15:32:52 +0000

    Add completed frontend project

---
+++ backend/src/test/java/com/oceaniq/ocean_iq_backend/OceanIqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceaniq.ocean_iq_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanIqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
