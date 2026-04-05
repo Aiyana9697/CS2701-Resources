9fd0d51081495ad99c8706069e964e583df42cbb
Aiyana Patel <aiyana.patel@outlook.com>
2026-04-05 19:27:57 +0100

    docs: add comments to include references

--- backend/src/main/java/com/oceaniq/OceanIqApplication.java
+++ backend/src/main/java/com/oceaniq/OceanIqApplication.java
@@ -4,6 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+/**
+ * Main application class for OceanIQ backend 
+ * entry point for Spring Boot application
+ * enables JPA auditing for automatic management of created/updated timestamps in entities
+ * 
+ * Inspiration and reference were taken from: 
+ * https://medium.com/@sonu.satyanand182/simplifying-audit-logging-in-spring-boot-with-enablejpaauditing-and-spring-security-31dab12c65b0
+*/
 @SpringBootApplication
 @EnableJpaAuditing
 public class OceanIqApplication {
