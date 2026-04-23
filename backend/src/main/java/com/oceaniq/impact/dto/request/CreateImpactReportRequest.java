3cf3cf02248a3d18e17f612cffa55e36225ae38e
M-Osman-249 <2423007@brunel.ac.uk>
2026-04-23 08:39:13 +0100

    Add CreateImpactReportRequest DTO for impact reports
    This class serves as a Data Transfer Object (DTO) for creating new impact reports, including fields for title, impact level, report type, and region ID.

---
+++ backend/src/main/java/com/oceaniq/impact/dto/request/CreateImpactReportRequest.java
@@ -0,0 +1,31 @@
+package com.oceaniq.impact.dto.request;
+
+import com.oceaniq.impact.enums.ImpactLevel;
+import com.oceaniq.impact.enums.ReportType;
+import lombok.Setter;
+import lombok.Getter;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * DTO for creating a new impact report.
+ * Contains necessary fields to create a report
+ * including title, impact level, report type and region
+ */
+@Setter
+@Getter
+public class CreateImpactReportRequest {
+
+    @NotBlank
+    private String title;
+
+    @NotNull
+    private ImpactLevel impact;
+
+    @NotNull
+    private ReportType reportType;
+
+    @NotNull
+    private Long regionId;
+}
