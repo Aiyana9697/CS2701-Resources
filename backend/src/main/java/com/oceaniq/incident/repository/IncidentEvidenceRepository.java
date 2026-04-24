c96dfd7b1e228c88144031aa077898f53fba78e6
Aiyana Patel <aiyana.patel@outlook.com>
2026-04-24 16:31:35 +0100

    feat(incident): update query to reference correct entity name

--- backend/src/main/java/com/oceaniq/incident/repository/IncidentEvidenceRepository.java
+++ backend/src/main/java/com/oceaniq/incident/repository/IncidentEvidenceRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface IncidentEvidenceRepository extends CrudRepository<IncidentEvidence, Integer> {
     // spring provides the built in methods without needed to write them down
-    @Query("SELECT f FROM ReportFile f WHERE f.fileName = ?1") // JPQL query
+    @Query("SELECT f FROM IncidentEvidence f WHERE f.fileName = ?1") // JPQL query
     IncidentEvidence findByFileName(String fileName); // the custom query to find file by its name
 }
